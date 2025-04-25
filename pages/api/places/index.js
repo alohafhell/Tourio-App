879390e5a162c49e85914b4878441cda67cb3d07
Daniel <cyubahirodaniel@gmail.com>
2025-04-25 11:16:28 +0200

    Add create place feature

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,6 +1,15 @@
-import { places } from "../../../lib/db";
+import dbConnect from '@/db/connect';
+import Place from '@/db/models/Place';
 
-export default function handler(request, response) {
-  response.status(200).json(places);
-  return;
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === 'POST') {
+    try {
+      await Place.create(request.body);
+      return response.status(201).json({'Message': 'Place created successfully'});
+    } catch (e) {
+      console.error(e);
+      return response.status(500).json({message: 'Error creating place'});
+    }
+  }
 }
