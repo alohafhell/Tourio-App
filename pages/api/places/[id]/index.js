4d6a363b6b8a9a96ba990bb2a794e9852b19b64d
Daniel <cyubahirodaniel@gmail.com>
2025-04-25 11:53:38 +0200

    Add delete feature

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,13 +1,21 @@
-import { places } from "../../../../lib/db.js";
+import {places} from '@/lib/db';
+import dbConnect from '@/db/connect';
+import Place from '@/db/models/Place';
 
-export default function handler(request, response) {
-  const { id } = request.query;
+export default async function handler(request, response) {
+  await dbConnect();
+  const {id} = request.query;
 
   const place = places.find((place) => place.id === id);
 
   if (!place) {
-    response.status(404).json({ status: "Not found" });
+    response.status(404).json({status: 'Not found'});
     return;
+  }
+
+  if (request.method === 'DELETE') {
+    await Place.findByIdAndDelete(id);
+    return response.status(200).json({status: 'Place deleted successfully'});
   }
 
   response.status(200).json(place);
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,9 +1,11 @@
 import dbConnect from '@/db/connect';
 import Place from '@/db/models/Place';
 import mongoose from "mongoose";
+import {places} from '@/lib/db';
 
 export default async function handler(request, response) {
   await dbConnect();
+  response.status(200).json(places);
   if (request.method === 'POST') {
     try {
       await Place.create(request.body);
